1be7282602a0047f72af0dc4f5eda10ca4114165
Brum <fbrump@gmail.com>
2018-11-12 22:48:09 -0200

    Created model for Company

--- hoursManagementSystem/company/models.py
+++ hoursManagementSystem/company/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    active = models.BooleanField(default=True)
+    date_create = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
