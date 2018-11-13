65113f9672dcda38a366e72aacbc3baf88afbb52
Brum <fbrump@gmail.com>
2018-11-12 22:53:53 -0200

    Added comment on unittest

--- hoursManagementSystem/company/tests.py
+++ hoursManagementSystem/company/tests.py
@@ -1,6 +1,13 @@
 from django.test import TestCase
 
+from .models import Company
+
 class CompaniesListTest(TestCase):
+    """
+        Unittest for list companies
+    """
+    def setup(self):
+        Company.objects.create(name='Sofware Solutions')
     def test_list_one_company_on_list(self):
         # arrange
         companies = []
