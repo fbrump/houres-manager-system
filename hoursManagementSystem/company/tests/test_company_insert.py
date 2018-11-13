f8a6664a926a05636f8a35679640ba13513df9b8
Brum <fbrump@gmail.com>
2018-11-12 23:26:58 -0200

    Moved all unittests cases for folder tests

--- hoursManagementSystem/company/tests/test_company_insert.py
+++ hoursManagementSystem/company/tests/test_company_insert.py
@@ -1,24 +1,7 @@
 from django.test import TestCase
 from datetime import datetime, timezone
 
-from .models import Company
-
-class CompaniesListTest(TestCase):
-    """
-        Unittest for list companies
-    """
-    def setUp(self):
-        """
-            Set up tests case
-        """
-        Company.objects.create(name='Sofware Solutions')
-    def test_list_one_company_on_list(self):
-        # arrange
-        companies = []
-        # act
-        companies = Company.objects.all()
-        # assert
-        self.assertTrue(0 < len(companies))
+from ..models import Company
 
 class CompanyInsertTest(TestCase):
     def setUp(self):
