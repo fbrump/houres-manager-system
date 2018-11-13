b05213e4e36387f1a85bd23ef19705979bbb3393
Brum <fbrump@gmail.com>
2018-11-12 22:53:34 -0200

    Allown description field black and null

--- hoursManagementSystem/company/models.py
+++ hoursManagementSystem/company/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Company(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     active = models.BooleanField(default=True)
     date_create = models.DateTimeField(auto_now_add=True)
     date_update = models.DateTimeField(auto_now=True)
