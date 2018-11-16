b80675dbf0086b7ec83d13f36b1b83b4515f2fcb
Brum <fbrump@gmail.com>
2018-11-15 22:35:21 -0200

    Created migrations and apply on datbase

--- hoursManagementSystem/pointsheets/models.py
+++ hoursManagementSystem/pointsheets/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from company.models import Company
+
+class Pointsheet(models.Model):
+    """
+    Description: Pointsheet
+    """
+    year = models.IntegerField()
+    month = models.IntegerField()
+    active = models.BooleanField(default=True)
+    date_create = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    class Meta:
+        pass
