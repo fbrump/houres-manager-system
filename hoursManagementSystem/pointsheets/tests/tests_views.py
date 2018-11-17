a04445266e6b9d0cbc41d664fe04b339d9cc1e62
Brum <fbrump@gmail.com>
2018-11-17 00:08:56 -0200

    Added other unittest for details pointsheet

--- hoursManagementSystem/pointsheets/tests/tests_views.py
+++ hoursManagementSystem/pointsheets/tests/tests_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from pointsheets.models import Pointsheet, Company
 
 class PointsheetViewsTest(TestCase):
 	"""docstring for PointsheetViewsTest"""
@@ -13,4 +14,10 @@ class PointsheetViewsTest(TestCase):
 	def test_success_pointsheet_index_html_filter_year(self):
 		year, month = 2018, 12
 		response = self.client.get('/pointsheets/{}/{}/'.format(year, month))
+		self.assertEqual(response.status_code, 200)
+	def test_success_pointsheet_show_details(self):
+		company =  Company.objects.create(name='Company 1')
+		pointsheet = Pointsheet.objects.create(year=2000, month=1, company=company)
+		pointsheet_id = pointsheet.id
+		response = self.client.get('/pointsheets/details/{}/'.format(pointsheet_id))
 		self.assertEqual(response.status_code, 200)
--- hoursManagementSystem/pointsheets/urls.py
+++ hoursManagementSystem/pointsheets/urls.py
@@ -8,5 +8,6 @@ app_name = 'pointsheets'
 urlpatterns = [
     path('', views.PointsheetListView.as_view(), name='index'),
     path('<int:year>/', views.PointsheetListView.as_view(), name='index-year'),
-    path('<int:year>/<int:month>/', views.PointsheetsDetailsView.as_view(), name='index-year-month'),
+    path('<int:year>/<int:month>/', views.PointsheetListView.as_view(), name='index-year-month'),
+    path('details/<int:pk>/', views.PointsheetsDetailsView.as_view(), name='index-details'),
 ]
