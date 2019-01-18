10d6bf80ae8e32f06fa869260bdb352b85f39c51
Brum <fbrump@gmail.com>
2019-01-18 22:43:35 +0000

    Added properties and comments

--- hoursManagementSystem/launches/models.py
+++ hoursManagementSystem/launches/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from pointsheets.models import Pointsheet
+
+class Launche(models.Model):
+    """
+        Description: Launche
+        fields:
+            date
+            time
+            active
+            date_create
+            date_update
+            pointsheet
+    """
+    date = models.DateField()
+    time = models.TimeField()
+    active = models.BooleanField(default=True)
+    date_create = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    pointsheet = models.ForeignKey(Pointsheet, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('date', 'time', 'pointsheet')
+    def __str__(self):
+        return '{} - {}'.format(self.date, self.time)
+
