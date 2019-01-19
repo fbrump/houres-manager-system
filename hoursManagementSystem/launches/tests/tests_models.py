a98ef39ea52d3bef66cb64ea418bdae619a9b1af
Brum <fbrump@gmail.com>
2019-01-19 00:01:13 +0000

    Created structure for LaunchesModel TestsCase

--- hoursManagementSystem/launches/tests/tests_models.py
+++ hoursManagementSystem/launches/tests/tests_models.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
-# Create your tests here.
+from ..models import Launche
+from company.models import Company
+from pointsheets.models import Pointsheet
+
+class LaunchesModelTests(TestCase):
+    """
+            docstring for LaunchesModelTests
+    """
+    def setUp(self):
+        self.company = Company.objects.create(name='Company Name')
+        pointsheet = Pointsheet.objects.create(
+            year='2018',
+            month='01',
+            company=self.company)
+    def test_create_new_launche():
+        pass
+
