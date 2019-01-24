683ff78444cf61fe8daf8e2ee0a542898e3d9f53
Brum <fbrump@gmail.com>
2019-01-24 23:01:35 +0000

    #1 #2 - Created unit test for testing view to create new launch. Setting for python

--- hoursManagementSystem/launches/tests/tests_views.py
+++ hoursManagementSystem/launches/tests/tests_views.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
-# Create your tests here.
+from company.models import Company
+from pointsheets.models import Pointsheet
+from ..models import Launch
+
+class LaunchViewsTest(TestCase):
+    def setUp(self):
+        self.company = Company.objects.create(name='Company Name')
+        self.pointsheet = Pointsheet.objects.create(
+            year='2018',
+            month='01',
+            company=self.company
+        )
+    def test_success_create_new_launch(self):
+        data = {
+            'date': '2010-01-02',
+            'time': '09:00',
+            'pointsheet_id': self.pointsheet.id
+        }
+        response = self.client.post('launches/', data=data)
+        self.assertEqual(response.status_code, 201)
