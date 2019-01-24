a1bd1c398c9fd9c15d3aa514caf77f6caa15fa86
Brum <fbrump@gmail.com>
2019-01-24 23:23:04 +0000

    #1 #2 - Crated url router to create launch, modelview and fixed unittest.skip anottation

--- hoursManagementSystem/launches/views.py
+++ hoursManagementSystem/launches/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+
+from .models import Launch
 
 def index(request):
     return render(request, 'launches/index.html')
+
+class LauncheCreateView(CreateView):
+    """
+        docstring for LauncheCreateView
+    """
+    model = Launch
+    fields = ['date', 'time', 'pointsheet']
+    template_name = 'launches/create.html'
+    success_url = reverse_lazy('launches:index')
+    def get_context_data(self, **kwargs):
+	    context = super().get_context_data(**kwargs)
+	    return context
