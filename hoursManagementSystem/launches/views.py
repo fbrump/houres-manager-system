9ab18d27659b78ab8ef1e518f3ba09fcbf2704d2
Brum <fbrump@gmail.com>
2019-07-23 00:13:41 +0100

    #2 #4 - Create list view and fix problem in redirect

--- hoursManagementSystem/launches/views.py
+++ hoursManagementSystem/launches/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 
 from .models import Launch
-
-def index(request):
-    return render(request, 'launches/index.html')
 
 class LauncheCreateView(CreateView):
     """
@@ -17,4 +15,16 @@ class LauncheCreateView(CreateView):
     success_url = reverse_lazy('launches:index')
     def get_context_data(self, **kwargs):
 	    context = super().get_context_data(**kwargs)
+	    return context
+
+class LauncheListView(ListView):
+    """
+        docstring for LauncheListView
+    """
+    model = Launch
+    paginated_by = 100
+    template_name = 'launches/index.html'
+    def get_context_data(self, **kwargs):
+	    context = super().get_context_data(**kwargs)
+	    print(context)
 	    return context
