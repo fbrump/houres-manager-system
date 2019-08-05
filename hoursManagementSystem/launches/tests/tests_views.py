7499328091776840f4a6e459ed1cef1e272c770c
Brum <fbrump@gmail.com>
2019-08-05 22:59:32 +0100

    Fix integration test: test_success_create_new_launch

--- hoursManagementSystem/launches/tests/tests_views.py
+++ hoursManagementSystem/launches/tests/tests_views.py
@@ -14,15 +14,15 @@ class LaunchViewsTest(TestCase):
             month='01',
             company=self.company
         )
-    @unittest.skip("search how can I test post view")
+    # @unittest.skip("search how can I test post view")
     def test_success_create_new_launch(self):
         data = {
             'date': '2010-01-02',
             'time': '09:00',
             'pointsheet_id': self.pointsheet.id
         }
-        response = self.client.get('launches/create/', data=data)
-        self.assertEqual(response.status_code, 201)
+        response = self.client.get('/launches/create/', data=data)
+        self.assertEqual(response.status_code, 200)
     @unittest.skip("it will work when fixed insert action")
     def test_try_to_create_a_new_launch_and_return_exception_identity(self):
         data = {
